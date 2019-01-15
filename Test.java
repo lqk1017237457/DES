9c1a1e299b85ee08d472cf035d7e15498a3815cd
lqklinqikai <43144475+lqklinqikai@users.noreply.github.com>
2019-01-15 22:14:53 +0800

    DES

---
+++ Test.java
@@ -0,0 +1,33 @@
+package DES;
+
+public class Test {
+	public static void main(String[] args) {
+		String str = "0011000100110010001100110011010000110101001101100011011100111000";
+		byte[] sKey = str.getBytes();
+		for (int i = 0; i < sKey.length; i++) 
+			sKey[i] -= '0';
+		System.out.print("密钥：");
+		printByteArr(sKey);
+		str = "0011000000110001001100100011001100110100001101010011011000110111";
+		byte[] plaintext = str.getBytes();
+		for (int i = 0; i < plaintext.length; i++) 
+			plaintext[i] -= '0';
+		System.out.print("明文：");
+		printByteArr(plaintext);
+		byte[] ciphertext = DES.encrypt(plaintext, sKey);
+		System.out.print("密文：");
+		printByteArr(ciphertext);
+		byte[] plainText = DES.decrypt(ciphertext, sKey);
+		System.out.print("明文：");
+		printByteArr(plainText);
+	}
+
+	static void printByteArr(byte[] b) {
+		for (int i = 0; i < b.length; i++) {
+			System.out.print(b[i]);
+			if (i % 8 == 7) 
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+}
